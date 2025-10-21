59c859d3cf26205c6d0c017f71b6f6e7b9346413
Alexander Rios <ijrios714@gmail.com>
2025-10-21 15:30:09 +0000

    y la imagen que va al incio es esta: https://lemonlearning.com/hubfs/Imp

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lemonlearning.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
