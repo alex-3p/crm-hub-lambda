75e1b7f6f12bd931fb6c140c2fb7708f87b541a1
Alexander Rios <ijrios714@gmail.com>
2025-11-11 09:12:35 +0000

    e el login que no diga Bienvenido de Neuvo si no Solo bienvenido y que n

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,10 +8,9 @@ export default function LoginPage() {
       <div className="w-full max-w-md p-8 space-y-8">
         <div className="flex flex-col items-center">
           <Link className="flex items-center justify-center mb-6" href="/">
-            <Image src="https://lambdaanalytics.co/wp-content/uploads/2024/10/Iso-LAMBDA-Blanco-Lima-Neon.png" width={28} height={28} alt="Lambda Analytics Logo" className="grayscale" />
-            <span className="ml-4 font-semibold text-2xl">Logos Gateway</span>
+            <Image src="https://lambdaanalytics.co/wp-content/uploads/2024/10/Iso-LAMBDA-Blanco-Lima-Neon.png" width={32} height={32} alt="Lambda Analytics Logo" className="grayscale" />
           </Link>
-          <h1 className="text-2xl font-bold text-center">Bienvenido de Nuevo</h1>
+          <h1 className="text-2xl font-bold text-center">Bienvenido</h1>
           <p className="text-muted-foreground text-center">
             Introduce tus credenciales para acceder a tu panel.
           </p>
